ea1778e6abff3a28917fe3c1e63958bbf4c716bd
harsnyi-main <harsanyitanulo@gmail.com>
2024-01-02 12:32:37 +0100

    Base models for the DB

--- warehouse_project_server/warehouse_project/models.py
+++ warehouse_project_server/warehouse_project/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+def validate_turning_day(value):
+    if value < 1 or value > 31:
+        raise ValidationError("Nem lehet kisebb a forduló nap mint 1, vagy nem lehet nagyobb mint 31.")
+
+def validate_area(value):
+    if value < 1 or value > 2000:
+        raise ValidationError("Nem lehet kisebb az alapterület mint 1, vagy nem lehet nagyobb mint 2000 négyzetméter.")
+
+def validate_cost(value):
+    if value < 1:
+        raise ValidationError("Nem lehet negatív a bérleti díj.")
+
+class Occupier(models.Model):
+    occupier_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    turning_day = models.IntegerField(validators=[validate_turning_day])
+    payment_method = models.BooleanField()
+    debt = models.BigIntegerField()
+    refreshed = models.DateField()
+
+class Storage(models.Model):
+    name = models.CharField(max_length=255)
+    area = models.IntegerField(validators=[validate_area()])
+    cost = models.IntegerField(validators=[validate_cost()])
+    occupier = models.OneToOneField(Occupier, on_delete=models.CASCADE)
+    comment = models.TextField()
